7063760442c2fd0b936af723e115d8c6af58ccaa
MarcusACWilliams <tue79528@temple.edu>
2019-09-08 00:36:33 -0400

    implementing decode method

--- decoder.js
+++ decoder.js
@@ -11,19 +11,40 @@ class Decoder {
             CharMap.forEach((value, key) => {
                 this.charMap.set(value, key);
             });
-
+        this.seenList = new Set();
         this.data;
         this.stack = [];
-        this.keyMax // Known max key value
-        this.leafCount // Each leaf represents a new possible combination 
+        this.keyMax; // Known max key value
+        this.leafCount = 0; // Each leaf represents a new possible combination 
     };
 
     // 1) push first char onto stack => 2) push n+1 char onto stack. this is the first branch
     // 3) use two-char key to find next possible value => if no key exist store key in keyMax => leafCount++
 
     decode (Data) {
-        this.data = Data;
-        stack.push(charMap(data[0]))
+        let i =1;
+        let childKey = "";
+        this.data = Data.split("");
+        this.stack.push(this.charMap.get(this.data[i-1]));
+
+        while(this.stack.length > 0) {
+
+            childKey = childKey.concat(this.data[i+1]);
+
+            if(this.charMap.get(parseInt(childKey)) != undefined) {
+                //Children of n are "n+1", "(n+1)(n+2)", etc while each child returns a valid key
+                this.stack.push(this.charMap.get(parseInt(childKey)));
+            }
+            else {
+                this.leafCount++;
+
+            }
+        }
     }
 
 }export default Decoder
+
+// 16 char alphabet map
+var alphabetMap = new Map();
+alphabetMap.set('a',1);alphabetMap.set('b',2);alphabetMap.set('c',3);alphabetMap.set('d',4);alphabetMap.set('e',5);alphabetMap.set('f',6);alphabetMap.set('g',7);alphabetMap.set('h',8);
+alphabetMap.set('i',9);alphabetMap.set('j',10);alphabetMap.set('k',11);alphabetMap.set('l',12);alphabetMap.set('m',13);alphabetMap.set('n',14);alphabetMap.set('o',15);alphabetMap.set('p',16);
--- test/decoder.test.js
+++ test/decoder.test.js
@@ -1,8 +1,45 @@
 import Decoder from "../decoder";
 import { exportAllDeclaration } from "@babel/types";
 
+// 16 char alphabet map
+const alphabetMap = new Map();
+alphabetMap.set('a',1);alphabetMap.set('b',2);alphabetMap.set('c',3);alphabetMap.set('d',4);alphabetMap.set('e',5);alphabetMap.set('f',6);alphabetMap.set('g',7);alphabetMap.set('h',8);
+alphabetMap.set('i',9);alphabetMap.set('j',10);alphabetMap.set('k',11);alphabetMap.set('l',12);alphabetMap.set('m',13);alphabetMap.set('n',14);alphabetMap.set('o',15);alphabetMap.set('p',16);
+
+// --Decoder(charMap)--
+
 test ('Key/Values get swapped correctly', () => {
     let testDecoder = new Decoder(new Map([ ['a', 1],['b',2] ]));
-    expect(testDecoder.charMap.keys ).toEqual([1,2]);
+    let keys = testDecoder.charMap.keys();
+    expect([keys.next().value,keys.next().value]).toEqual([1,2]);
+});
+
+test ('Values Alpha values are returned correctly from new numeric keys', () => {
+    let testDecoder = new Decoder(new Map([ ['a', 1],['b',2] ]));
+    let testMap = testDecoder.charMap;
+    expect([testMap.get(1),testMap.get(2)]).toEqual(['a','b']);
 
 });
+
+test ('Input Map passed in correctly', () => {
+    let testMap = new Map([ ['a', 1],['b',2] ] );
+    let testDecoder = new Decoder(new Map([ ['a', 1],['b',2] ] ));
+    expect(testMap).toEqual(testDecoder.inputMap);
+
+});
+
+test ('Message Fully Decoded', () => { 
+    let data = "12345";
+    
+    let testDecoder = new Decoder(alphabetMap);
+    testDecoder.decode(data);
+    expect(testDecoder.leafCount).toEqual(2);
+
+});
+
+/*
+1 - 2 - 3 - 4 - 5 abcde
+12 - 3 - 4 - 5  lcde
+x123x
+
+*/
